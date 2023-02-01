c1d12881c9d621cbf35cd66865f430fd9f7cab10
bhiwde <nirmlarameshbhiwde@gmail.com>
2023-02-01 14:30:42 +0530

    [ADDED];[NIRMLA] Even or Odd numbers in c

---
+++ EVENODD.C
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ #include<conio.h>
+void main()
+{
+	int n;
+	clrscr();
+	printf("Enter Any Number:");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+	printf("This Is Even Number");
+	}
+	else
+	{
+	printf("This Is Odd Number");
+	}
+	getch();
+	}
+
